9fe3471de3058637f16baba70e7cc1006ea90543
Emilse <emilsearias@gmail.com>
2021-09-30 09:17:14 -0300

    se agrego el archivo de clase

---
+++ js/admin.js
@@ -0,0 +1,12 @@
+// este archivo tendra toda la logica del ABM o CRUD
+let producto = document.querySelector("#producto");
+let cantidad = document.querySelector("#cantidad");
+let codigo = document.querySelector("#codigo");
+let descripcion = document.querySelector("#descripcion");
+let url = document.querySelector("#url");
+let formulario = document.querySelector("#formProducto");
+//  console.log(formulario);
+
+producto.addEventListener("blur", () => {
+    validarCampoRequerido(producto);
+  });
